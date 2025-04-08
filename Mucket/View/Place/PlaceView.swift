46fdf15f3d5bca205b20f2a96f3ed9235fc1972b
Woohyun Jo <bluewing1004@gmail.com>
2025-04-08 20:31:01 +0900

    [Feat] 지도 뷰 구성 변경

--- Mucket/View/Place/PlaceView.swift
+++ Mucket/View/Place/PlaceView.swift
@@ -7,7 +7,34 @@
 
 import UIKit
 import SnapKit
+import KakaoMapsSDK
 
 final class PlaceView: BaseView {
+    let mapView = KMViewContainer()
+    let currentLocationButton = UIButton()
     
+    override func configureHierarchy() {
+        [mapView, currentLocationButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        currentLocationButton.snp.makeConstraints { make in
+            make.bottom.trailing.equalTo(safeAreaLayoutGuide).inset(16)
+            make.size.equalTo(50)
+        }
+    }
+    
+    override func configureView() {
+        currentLocationButton.layer.cornerRadius = 25
+        currentLocationButton.clipsToBounds = true
+        currentLocationButton.backgroundColor = .themePrimary
+        currentLocationButton.setImage(.location, for: .normal)
+        currentLocationButton.tintColor = .backgroundPrimary
+    }
 }
