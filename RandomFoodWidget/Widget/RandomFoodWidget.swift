34c0a7965ef4bf690cb9fd2a969fc91caafe3a36
Woohyun Jo <bluewing1004@gmail.com>
2025-04-23 18:49:09 +0900

    [Feat] 위젯 이름, 설명, 크기 수정

--- RandomFoodWidget/Widget/RandomFoodWidget.swift
+++ RandomFoodWidget/Widget/RandomFoodWidget.swift
@@ -22,8 +22,9 @@ struct RandomFoodWidget: Widget {
                     .background(.backgroundPrimary)
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .supportedFamilies([.systemSmall, .systemMedium])
+        .configurationDisplayName("오늘 뭐먹지?")
+        .description("추천 요리를 랜덤으로 보여줍니다. 레시피 검색하기 버튼을 통해 바로 검색할 수 있습니다.")
     }
 }
 
